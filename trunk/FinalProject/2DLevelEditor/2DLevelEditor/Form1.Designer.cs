72c9eeaa83ff1f34ef8d7984425e024bbcbb3f20
frezarion <frezarion@d0efb9f9-cd0b-a185-d6be-c2abaafb4ae4>
2011-04-11 21:56:42 +0000

    GUI Working Now

--- trunk/FinalProject/2DLevelEditor/2DLevelEditor/Form1.Designer.cs
+++ trunk/FinalProject/2DLevelEditor/2DLevelEditor/Form1.Designer.cs
@@ -32,7 +32,7 @@
             System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Stationary Enemy");
             System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Walking Enemy");
             System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Jumping Enemy");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Tough Enemy", 7, 7);
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Tough Enemy");
             System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Enemies", new System.Windows.Forms.TreeNode[] {
             treeNode1,
             treeNode2,
@@ -51,13 +51,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newLevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openLevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveLevelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveGameToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -90,64 +86,37 @@
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newGameToolStripMenuItem,
             this.newLevelToolStripMenuItem,
-            this.openGameToolStripMenuItem,
             this.openLevelToolStripMenuItem,
-            this.saveGameToolStripMenuItem,
             this.saveLevelToolStripMenuItem,
-            this.saveGameToolStripMenuItem1,
             this.exitToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
             this.toolStripMenuItem1.Text = "File";
             // 
-            // newGameToolStripMenuItem
-            // 
-            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
-            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
-            this.newGameToolStripMenuItem.Text = "New Game";
-            // 
             // newLevelToolStripMenuItem
             // 
             this.newLevelToolStripMenuItem.Name = "newLevelToolStripMenuItem";
-            this.newLevelToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.newLevelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.newLevelToolStripMenuItem.Text = "New Level";
-            // 
-            // openGameToolStripMenuItem
-            // 
-            this.openGameToolStripMenuItem.Name = "openGameToolStripMenuItem";
-            this.openGameToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
-            this.openGameToolStripMenuItem.Text = "Open Game";
+            this.newLevelToolStripMenuItem.Click += new System.EventHandler(this.newLevelToolStripMenuItem_Click);
             // 
             // openLevelToolStripMenuItem
             // 
             this.openLevelToolStripMenuItem.Name = "openLevelToolStripMenuItem";
-            this.openLevelToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.openLevelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.openLevelToolStripMenuItem.Text = "Open Level";
-            // 
-            // saveGameToolStripMenuItem
-            // 
-            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
-            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
-            this.saveGameToolStripMenuItem.Text = "Save Game";
             // 
             // saveLevelToolStripMenuItem
             // 
             this.saveLevelToolStripMenuItem.Name = "saveLevelToolStripMenuItem";
-            this.saveLevelToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.saveLevelToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.saveLevelToolStripMenuItem.Text = "Save Level";
-            // 
-            // saveGameToolStripMenuItem1
-            // 
-            this.saveGameToolStripMenuItem1.Name = "saveGameToolStripMenuItem1";
-            this.saveGameToolStripMenuItem1.Size = new System.Drawing.Size(157, 22);
-            this.saveGameToolStripMenuItem1.Text = "Save Game As...";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // editToolStripMenuItem
@@ -176,9 +145,6 @@
             // 
             // treeView1
             // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.treeView1.BackColor = System.Drawing.SystemColors.Control;
             this.treeView1.FullRowSelect = true;
             this.treeView1.ImageIndex = 0;
@@ -193,13 +159,13 @@
             treeNode2.Name = "WalkingEnemy";
             treeNode2.SelectedImageKey = "yellow.png";
             treeNode2.Text = "Walking Enemy";
-            treeNode3.ImageIndex = 4;
+            treeNode3.ImageKey = "orange.png";
             treeNode3.Name = "JumpingEnemy";
             treeNode3.SelectedImageKey = "orange.png";
             treeNode3.Text = "Jumping Enemy";
-            treeNode4.ImageIndex = 7;
+            treeNode4.ImageKey = "red.png";
             treeNode4.Name = "ToughEnemy";
-            treeNode4.SelectedImageIndex = 7;
+            treeNode4.SelectedImageKey = "red.png";
             treeNode4.Text = "Tough Enemy";
             treeNode5.ImageKey = "red.png";
             treeNode5.Name = "Enemies";
@@ -234,7 +200,7 @@
             treeNode9,
             treeNode11});
             this.treeView1.SelectedImageIndex = 0;
-            this.treeView1.Size = new System.Drawing.Size(168, 481);
+            this.treeView1.Size = new System.Drawing.Size(168, 638);
             this.treeView1.TabIndex = 3;
             // 
             // imageList1
@@ -257,7 +223,7 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(960, 480);
+            this.panel1.Size = new System.Drawing.Size(960, 640);
             this.panel1.TabIndex = 4;
             // 
             // pictureBox1
@@ -266,20 +232,23 @@
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(960, 480);
+            this.pictureBox1.Size = new System.Drawing.Size(960, 637);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1131, 505);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1131, 662);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.menuStrip1);
@@ -303,13 +272,9 @@
         private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem newLevelToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openLevelToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveLevelToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveGameToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.ImageList imageList1;
--- trunk/FinalProject/2DLevelEditor/2DLevelEditor/Form1.cs
+++ trunk/FinalProject/2DLevelEditor/2DLevelEditor/Form1.cs
@@ -9,11 +9,25 @@ using System.Windows.Forms;
 
 namespace _2DLevelEditor
 {
+
     public partial class Form1 : Form
     {
+        int width;
+        int height;
+        Tile[,] board;
         public Form1()
         {
             InitializeComponent();
+            width = 30;
+            height = 20;
+            board = new Tile[height, width];
+            for(int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    board[i,j] = new Tile();
+                }
+            }
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
@@ -24,6 +38,58 @@ namespace _2DLevelEditor
         private void solidPlatformToolStripMenuItem_MouseDown(object sender, MouseEventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            TreeNode current = treeView1.SelectedNode;
+            int X = e.X / 32;
+            int Y = e.Y / 32;
+            board[Y, X].ImageFile = current.SelectedImageKey;
+            board[Y, X].TileType = current.Text;
+            Image pic = pictureBox1.Image;
+            if (pic == null)
+            {
+                pic = new Bitmap(width*32, height*32);
+            }
+            Graphics level = Graphics.FromImage(pic);
+            Bitmap tile = new Bitmap("../../../" + current.SelectedImageKey);
+            Rectangle area = new Rectangle(X*32, Y*32, 32, 32);
+            level.DrawImage(tile, area);
+            pictureBox1.Image = pic;
+        }
+
+        private void newLevelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewLevel newlevel = new NewLevel();
+            DialogResult result = newlevel.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                width = newlevel.FindWidth;
+                height = newlevel.FindHeight;
+                string bgimage = newlevel.FindImage;
+                if (bgimage != "")
+                {
+                    pictureBox1.BackgroundImage = new Bitmap(bgimage);
+                }
+                this.Text = newlevel.FindName;
+                FormSet(width*32, height*32);
+            }
+        }
+
+        private void FormSet(int w, int h)
+        {
+            panel1.Height = h;
+            panel1.Width = w;
+            pictureBox1.Height = h;
+            pictureBox1.Width = w;
+            treeView1.Height = h;
+            treeView1.Left = w + 1;
         }
     }
 }
